4516fa17e5d9561791663cb987207db3323e01c9
Funky MC <mcjazzyfunky@gmail.com>
2016-02-15 19:54:15 +0000

    Fixed exporting bug

--- index.js
+++ index.js
@@ -4,10 +4,21 @@ import Arrays from './src/main/Arrays.js';
 import Seq from './src/main/Seq.js';
 import Reader from './src/main/Reader.js'
 
+export {
+    Objects,
+    Strings,
+    Arrays,
+    Seq,
+    Reader
+};
 
-const jsprelude = { Objects, Strings, Arrays, Seq, Reader };
-
-export default jsprelude;
+const jsprelude = {
+    Objects,
+    Strings,
+    Arrays,
+    Seq,
+    Reader
+};
 
 if (typeof define === 'function' && define.amd) {
     define(() => jsprelude);
